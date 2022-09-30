8d4c32c11bcfcbb4ae4f85ced5a21a568deb9f73
SMA1972 <smisbahali@gmail.com>
2022-09-30 12:13:24 +0500

    w2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flut1ter Demo',
+      title: 'Flut1ter Dem33o',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Fluttereee12 Demo Home Page'),
     );
   }
 }
